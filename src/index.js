098048f2b1a06b2a99a0a3d575678a9922881e2b
Jaeuk Lim <applewodnr@naver.com>
2021-12-21 21:48:29 +0900

    style: 메인 페이지를 위한 index.scss 반영

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { $, add } from '@/util';
+import '@/styles/index.scss';
 
 const $target = $('#root');
 
 const template = `
-    <h1> 1 + 2 = ${add(1, 2)}</h1>
+	<h1>Hello webpack-boilerplate</h1>
+	<h2> 1 + 2 = ${add(1, 2)}</h2>
+	<h3>mugglim</h3>
 `;
 
 // 폴리필 트랜스파일링 테스트 용
